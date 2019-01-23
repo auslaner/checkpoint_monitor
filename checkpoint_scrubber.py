4c235acd0db9933afeb02dba40e29f5eb7386bab
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2019-01-23 14:14:09 -0700

    Checkpoints in Separate Model Directories
    The script now expects a file path containing multiple model
    directories, each of which contain their own checkpoint files.
    
    Existing tests were modified to test for this change in functionality.
    
    Signed-off-by: Avery Uslaner <avery.uslaner@redbutte.utah.edu>

--- checkpoint_scrubber.py
+++ checkpoint_scrubber.py
@@ -13,21 +13,27 @@ import os
 MILESTONE_CHKPTS = [50, 60, 70, 80]
 
 
-def main(checkpoint_path):
-    # Create a dictionary of filenames with their checkpoint numbers
-    checkpoints = {filename: int(''.join(filter(str.isdigit, filename)))
-                   for filename in os.listdir(checkpoint_path) if ''.join(filter(str.isdigit, filename)).isdigit()}
+def main(model_path):
+    for dirpath, dirnames, filenames in os.walk(model_path):
+        # Create a dictionary of filenames with their checkpoint numbers
+        checkpoints = {filename: int(''.join(filter(str.isdigit, filename.split("-")[-1])))
+                       for filename in filenames if ''.join(filter(str.isdigit, filename.split("-")[-1])).isdigit()}
 
-    chkpt_nums = checkpoints.values()
-    end_chkpt_num = max(chkpt_nums)
+        chkpt_nums = checkpoints.values()
+        try:
+            end_chkpt_num = max(chkpt_nums)
+        except ValueError:
+            # No checkpoints
+            continue
 
-    chkpts_to_keep = MILESTONE_CHKPTS.copy()
-    chkpts_to_keep.append(end_chkpt_num)
+        chkpts_to_keep = MILESTONE_CHKPTS.copy()
+        chkpts_to_keep.append(end_chkpt_num)
 
-    for fname, chkpt_num in checkpoints.items():
-        if chkpt_num not in chkpts_to_keep:
-            print("Deleting {}...".format(fname))
-            os.remove(os.path.join(checkpoint_path, fname))
+        for fname, chkpt_num in checkpoints.items():
+            if chkpt_num not in chkpts_to_keep:
+                print("Checkpoints to keep:", chkpts_to_keep)
+                print("Deleting {}...".format(fname))
+                os.remove(os.path.join(dirpath, fname))
 
 
 if __name__ == "__main__":
--- tests/test_scrubber.py
+++ tests/test_scrubber.py
@@ -6,28 +6,89 @@ from checkpoint_scrubber import main
 
 @pytest.fixture
 def sample_fpath(tmp_path):
-    sample_files = ["vggnet-001.params",
-                    "vggnet-0010.params",
-                    "vggnet-0011.params",
-                    "vggnet-0012.params",
-                    "vggnet-0013.params",
-                    "vggnet-0014.params",
-                    "vggnet-0050.params",
-                    "vggnet-0093.params",
-                    "vggnet-symbol.json"]
-    for fname in sample_files:
-        p = tmp_path / fname
-        p.touch()
+    def populate_test_directories(sample_files, model_directory):
+        for fname in sample_files:
+            p = model_directory / fname
+            p.touch()
+
+    model_1_files = ["model1-001.params",
+                     "model1-0010.params",
+                     "model1-0011.params",
+                     "model1-0012.params",
+                     "model1-0013.params",
+                     "model1-0014.params",
+                     "model1-0050.params",
+                     "model1-0093.params",
+                     "model1-symbol.json"]
+
+    model_2_files = ["model2-002.params",
+                     "model2-0011.params",
+                     "model2-0012.params",
+                     "model2-0017.params",
+                     "model2-0051.params",
+                     "model2-0052.params",
+                     "model2-symbol.json"]
+
+    model_3_files = ["model3-0010.params",
+                     "model3-0060.params",
+                     "model3-0070.params",
+                     "model3-symbol.json"]
+
+    model_1 = tmp_path / "model_1"
+    model_2 = tmp_path / "model_2"
+    model_3 = tmp_path / "model_3"
+
+    model_1.mkdir()
+    model_2.mkdir()
+    model_3.mkdir()
+
+    populate_test_directories(model_1_files, model_1)
+    populate_test_directories(model_2_files, model_2)
+    populate_test_directories(model_3_files, model_3)
 
     return tmp_path
 
 
 def test_main(sample_fpath):
+    # Test to make sure the sample files are setup correctly
+    model_dirs = os.listdir(sample_fpath)
+    assert "model_1" in model_dirs
+    assert "model_2" in model_dirs
+    assert "model_3" in model_dirs
+
+    for dirpath, dirnames, filenames in os.walk(sample_fpath):
+        print("Testing {}".format(dirpath))
+        print("It contains the files:\n\t{}".format(filenames))
+
+        if dirpath.split(os.sep)[-1] == "model_1":
+            assert len(filenames) == 9
+            assert "model1-001.params" in filenames
+            assert "model1-0012.params" in filenames
+
+        if dirpath.split(os.sep)[-1] == "model_2":
+            assert len(filenames) == 7
+            assert "model2-symbol.json" in filenames
+            assert "model2-0017.params" in filenames
+
+    print("*" * 90)
+    print("[!] Running main function from checkpoint_scrubber...")
+    # Test the function
     main(sample_fpath)
-    files = os.listdir(sample_fpath)
-    assert len(files) == 3
-    assert "vggnet-001.params" not in files
-    assert "vggnet-0010.params" not in files
-    assert "vggnet-0050.params" in files
-    assert "vggnet-0093.params" in files
-    assert "vggnet-symbol.json" in files
+    print("[!] Done!")
+    print("*" * 90)
+
+    for dirpath, dirnames, filenames in os.walk(sample_fpath):
+        print("Testing {}".format(dirpath))
+        print("It contains the files:\n\t{}".format(filenames))
+
+        if dirpath.split(os.sep)[-1] == "model_1":
+            assert len(filenames) == 3
+            assert "model1-001.params" not in filenames
+            assert "model1-0012.params" not in filenames
+            assert "model1-0093.params" in filenames
+            assert "model1-symbol.json" in filenames
+
+        if dirpath.split(os.sep)[-1] == "model_2":
+            assert len(filenames) == 2
+            assert "model2-0017.params" not in filenames
+            assert "model2-symbol.json" in filenames
